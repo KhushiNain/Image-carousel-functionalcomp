5ffa121e8a756838d863e7d595e9d4d6088c5080
KhushiNain <146444846+KhushiNain@users.noreply.github.com>
2024-01-08 17:06:58 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,31 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+
+    const [index,setIndex]=useState(0);
+
+    const handleBackArrow=()=>{
+        if(index==0){
+            setIndex(images.length-1)
+        }else{
+            setIndex((prev)=>prev-1)
+        }
+    }
+    const handleForwardArrow=()=>{
+        index==2?setIndex(0):setIndex((prev)=>prev+1)  
+    }
+
+    return(
+        // empty react fragment to wrap all the elements)
+        <>
+        <img className="image" src={images[index].img} alt="" />
+        <ArrowBackIosIcon className="button left-btn" onClick={handleBackArrow}/>
+        <ArrowForwardIosIcon className="button right-btn" onClick={handleForwardArrow}/>
+        <h1 className="heading text">{images[index].title}</h1>
+        <p className="text subheading">{images[index].subtitle}</p>
+
+        </>
+    )
 }
 
 export default Carousel;
